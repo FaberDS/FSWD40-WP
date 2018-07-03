24238ea0133a5d56a86092cc3450eb77a49625bc
Denis Schuele <schueledenis@gmx.at>
2018-07-03 15:46:57 +0200

    basic template

---
+++ blog.php
@@ -0,0 +1,13 @@
+<div class="row blog-post">
+    <div class="col-md-4 col-sm-12 blog-container-outside">
+        <div class="blog-container-inside">
+            <h2 class="blog-post-title"><?php the_title();?></h2>
+            <p class="blog-post-meta"><?php the_date();?> by <a href="#"><?php the_author();?></a></p>
+            <?php the_content(); ?>
+        </div>
+    </div>
+    
+            
+</div><!-- /.blog-post -->
+
+        
---
+++ content.php
@@ -0,0 +1,13 @@
+<div class="row blog-post">
+    <div class="col-md-4 col-sm-12 content-container-outside">
+        <div class="content-container-inside">
+            <h2 class="blog-post-title"><?php the_title();?></h2>
+            <p class="blog-post-meta"><?php the_date();?> by <a href="#"><?php the_author();?></a></p>
+            <?php the_content(); ?>
+        </div>
+    </div>
+    
+            
+</div><!-- /.blog-post -->
+
+        
